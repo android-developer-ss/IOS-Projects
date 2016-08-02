4e7c2b43c9603de3381ff9186e2c5c03ed21c9b5
Snehal Sutar <svs.sutar.1@gmail.com>
2016-08-01 23:03:26 -0500

    switch working with all components

--- SwitchSample/SwitchSample/ViewController.swift
+++ SwitchSample/SwitchSample/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIImageView!
+    @IBOutlet weak var redColorSwitch: UISwitch!
+    @IBOutlet weak var greenColorSwitch: UISwitch!
+    @IBOutlet weak var blueColorSwitch: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func changeColorComponent(sender: AnyObject) {
+        
+        let r: CGFloat = self.redColorSwitch.on ? 1 : 0
+        let g: CGFloat = self.greenColorSwitch.on ? 1 : 0
+        let b: CGFloat = self.blueColorSwitch.on ? 1 : 0
+        
+        colorView.backgroundColor = UIColor(red: r, green:g, blue:b, alpha: 1)
+    }
 }
 
