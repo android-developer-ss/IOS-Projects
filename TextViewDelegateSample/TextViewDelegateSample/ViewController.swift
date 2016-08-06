976dc928893de1745e3f0ec205dbf541522f4814
Snehal Sutar <svs.sutar.1@gmail.com>
2016-08-06 11:28:11 -0500

    added text fields on storyboard and their controllers in view controller

--- TextViewDelegateSample/TextViewDelegateSample/ViewController.swift
+++ TextViewDelegateSample/TextViewDelegateSample/ViewController.swift
@@ -8,16 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    // Textfield and label outlets
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var characterCountLabel: UILabel!
+    
+    // Delegate instances
+    let emojiDelegate = EmojiTextFieldDelegate()
+    let colorizerDelegate = ColorizerTextFieldDelegate()
+   
+    //Lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.textField1.delegate = emojiDelegate
+        self.textField2.delegate = colorizerDelegate
+        self.textField3.delegate = self
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        var newText : NSString = textField.text!
+        
+        newText = newText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        self.characterCountLabel.hidden = (newText.length == 0)
+        
+        self.characterCountLabel.text = String(newText.length)
+        
+        // Returning true gives permission to modify the text in the textfield.
+        return true;
     }
 
 
