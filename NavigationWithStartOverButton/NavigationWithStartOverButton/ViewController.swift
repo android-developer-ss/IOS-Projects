b25cf8ae2b3b2f3a7302690c4285e65585cf5e95
Snehal Sutar <svs.sutar.1@gmail.com>
2016-08-10 20:30:03 -0500

    code to set start over button on all screens - by adding same view controller class for all the view controllers in the story board

--- NavigationWithStartOverButton/NavigationWithStartOverButton/ViewController.swift
+++ NavigationWithStartOverButton/NavigationWithStartOverButton/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem (
+            title: "Start Over",
+            style: UIBarButtonItemStyle.Plain,
+            target: self,
+            action: #selector(ViewController.startOver))
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func startOver() {
+        
+        if let navigationController = self.navigationController {
+            navigationController.popToRootViewControllerAnimated(true)
+        }
+    }
 
 }
 
