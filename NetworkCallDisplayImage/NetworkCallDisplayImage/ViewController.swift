2b973c9a986031ec7b440fb7fdac3f699c4cfaf2
Snehal Sutar <svs.sutar.1@gmail.com>
2016-08-11 20:23:36 -0500

    added image and code to convert image into UImage and display on story board

--- NetworkCallDisplayImage/NetworkCallDisplayImage/ViewController.swift
+++ NetworkCallDisplayImage/NetworkCallDisplayImage/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let imageUrl = NSURL(string: "https://vignette3.wikia.nocookie.net/dfairies/images/0/08/Tinker_Bell_(Disney_Fairies).jpg")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(imageUrl) {
+            (data , response, error) in
+            if (error == nil){
+                let downloadedImage = UIImage(data:data!)
+                self.imageView.image = downloadedImage
+            }
+        }
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
