14501de22a012f7ed1767fe2ca059062c31e5940
Snehal Sutar <snehal.v.sutar@gmail.com>
2016-08-18 21:57:46 -0500

    Added outlets and actions

--- GettingImagesFromFlickr/GettingImagesFromFlickr/ViewController.swift
+++ GettingImagesFromFlickr/GettingImagesFromFlickr/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageName: UITextField!
+    @IBOutlet weak var getNextImageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func getNextImageAction(sender: AnyObject) {
+    }
 
 }
 
