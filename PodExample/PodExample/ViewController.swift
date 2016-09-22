5d0fd6b560128f91a5423ac27476bb329f5e21a3
Snehal Sutar <svs.sutar.1@gmail.com>
2016-09-22 17:06:56 -0500

    Init pod example.

---
+++ PodExample/PodExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PodExample
+//
+//  Created by Snehal Sutar on 9/22/16.
+//  Copyright © 2016 Snehal Sutar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
