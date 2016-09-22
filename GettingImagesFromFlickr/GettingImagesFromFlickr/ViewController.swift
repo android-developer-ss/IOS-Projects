9f7122814f6286f2109ef780c55472c6e6ff3c17
Snehal Sutar <svs.sutar.1@gmail.com>
2016-09-22 17:06:17 -0500

    Added escaped parameters method to viewController.

--- GettingImagesFromFlickr/GettingImagesFromFlickr/ViewController.swift
+++ GettingImagesFromFlickr/GettingImagesFromFlickr/ViewController.swift
@@ -25,6 +25,46 @@ class ViewController: UIViewController {
     }
 
     @IBAction func getNextImageAction(_ sender: AnyObject) {
+        setUIEnabled(false)
+        getImageFromFlickr()
+    }
+
+    fileprivate func setUIEnabled(_ enabled: Bool) {
+        imageName.isEnabled = enabled
+        getNextImageButton.isEnabled = enabled
+        
+        if enabled {
+            getNextImageButton.alpha = 1.0
+        } else {
+            getNextImageButton.alpha = 0.5
+        }
+    }
+    
+    
+    
+    
+    fileprivate func escapedParameters(_ parameters: [String:AnyObject]) -> String {
+        
+        if parameters.isEmpty {
+            return ""
+        } else {
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                
+                // make sure that it is a string value
+                let stringValue = "\(value)"
+                
+                // escape it
+                let escapedValue = stringValue.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+                
+                // append it
+                keyValuePairs.append(key + "=" + "\(escapedValue!)")
+                
+            }
+            
+            return "?\(keyValuePairs.joined(separator: "&"))"
+        }
     }
 
 }
