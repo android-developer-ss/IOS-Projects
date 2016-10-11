2940cccae05d5d8a7556b03db9aec8892ab73570
Snehal Sutar <svs.sutar.1@gmail.com>
2016-10-11 09:25:57 -0500

    assets added

--- ScrollViewSample/ScrollViewSample/ViewController.swift
+++ ScrollViewSample/ScrollViewSample/ViewController.swift
@@ -9,10 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var images = [UIImageView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for x in 0...2{
+            let image = UIImage(named: "icon\(x).png")
+            images.append(UIImageView(image:image))
+        }
     }
 
     override func didReceiveMemoryWarning() {
