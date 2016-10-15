faaeb608c1cc70da9f7f4f4871e951da89663cd0
Snehal Sutar <svs.sutar.1@gmail.com>
2016-10-15 18:31:06 -0500

    added logic to play sound on button press.

--- RetroCalculator/RetroCalculator/ViewController.swift
+++ RetroCalculator/RetroCalculator/ViewController.swift
@@ -7,17 +7,40 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var buttonSound:AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let path = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: path!)
+        
+        do{
+            try buttonSound = AVAudioPlayer(contentsOf: soundURL)
+            buttonSound.prepareToPlay()
+        }catch let err as NSError{
+            print(err.debugDescription)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func numberPressed(sender: UIButton){
+        
+    }
+    
+    func playSound(){
+        if(buttonSound.isPlaying){
+            buttonSound.stop()
+        }
     }
 
 
