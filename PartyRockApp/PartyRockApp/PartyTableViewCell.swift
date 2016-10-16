92fd4a945610f023e45dda383fc6716701554c9f
Snehal Sutar <svs.sutar.1@gmail.com>
2016-10-16 11:53:06 -0500

    Added groups for model, view and controller

---
+++ PartyRockApp/PartyRockApp/PartyTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PartyTableViewCell.swift
+//  PartyRockApp
+//
+//  Created by Snehal Sutar on 10/16/16.
+//  Copyright © 2016 Snehal Sutar. All rights reserved.
+//
+
+import UIKit
+
+class PartyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
