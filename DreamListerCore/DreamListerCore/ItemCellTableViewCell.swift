faebe9447f3c515908b3f40e7daf5fc9022dc4d8
Snehal Sutar <svs.sutar.1@gmail.com>
2016-10-19 10:36:09 -0500

    Added assets

---
+++ DreamListerCore/DreamListerCore/ItemCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCellTableViewCell.swift
+//  DreamListerCore
+//
+//  Created by Snehal Sutar on 10/18/16.
+//  Copyright © 2016 Snehal Sutar. All rights reserved.
+//
+
+import UIKit
+
+class ItemCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ DreamListerCore/DreamListerCore/MaterialView.swift
@@ -0,0 +1,21 @@
+//
+//  MaterialView.swift
+//  DreamListerCore
+//
+//  Created by Snehal Sutar on 10/18/16.
+//  Copyright © 2016 Snehal Sutar. All rights reserved.
+//
+
+import UIKit
+
+class MaterialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
