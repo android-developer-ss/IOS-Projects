3c4765a02ac8e534c88ea983697bb19fbf0dd88a
Snehal Sutar <svs.sutar.1@gmail.com>
2016-10-19 10:37:33 -0500

    Implemented table view delegates and datasource and some other changes.

--- DreamListerCore/DreamListerCore/MainVC.swift
+++ DreamListerCore/DreamListerCore/MainVC.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var segment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
 
 }
 
