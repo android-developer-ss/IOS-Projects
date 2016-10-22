ef3c768697f74ddfd3a199bd50ce7e0bb739c14e
Snehal Sutar <svs.sutar.1@gmail.com>
2016-10-22 18:00:44 -0500

    Logic for controller and setting data into table view cells. Also generating some test data.

--- DreamListerCore/DreamListerCore/MainVC.swift
+++ DreamListerCore/DreamListerCore/MainVC.swift
@@ -20,6 +20,9 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        generateTestData()
+        attemptFetch()
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -28,13 +31,33 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCellTableViewCell
+        configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
+        return cell
     }
+    
+    func configureCell(cell: ItemCellTableViewCell, indexPath: NSIndexPath){
+        //update cell
+        let item = controller.object(at: indexPath as IndexPath)
+        cell.configureCell(item: item)
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let sections = controller.sections {
+            let sectionInfo = sections[section]
+            return sectionInfo.numberOfObjects
+        }
         return 0
     }
     func numberOfSections(in tableView: UITableView) -> Int {
+        if let sections = controller.sections {
+            return sections.count
+        }
         return 0
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
     
     func attemptFetch(){
@@ -42,8 +65,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
         let dateSort = NSSortDescriptor(key: "created", ascending: false)
         fetchRequest.sortDescriptors = [dateSort]
         
-        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context,
-                                                    sectionNameKeyPath: nil, cacheName: nil)
+        let controller = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: context,sectionNameKeyPath: nil, cacheName: nil)
+        self.controller = controller
         
         do{
             try controller.performFetch()
@@ -73,8 +96,8 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
             }
         case .update:
             if let indexPath = newIndexPath {
-                let cell = tableView.cellForRow(at: indexPath) as? ItemCellTableViewCell
-                // update the cell data
+                let cell = tableView.cellForRow(at: indexPath) as! ItemCellTableViewCell
+                configureCell(cell: cell, indexPath: indexPath as NSIndexPath)
             }
         case .move:
             if let indexPath = indexPath {
@@ -84,6 +107,25 @@ class MainVC: UIViewController, UITableViewDelegate, UITableViewDataSource, NSFe
                 tableView.insertRows(at: [indexPath], with: .fade)
             }
         }
+    }
+    
+    func generateTestData(){
+        let item = Item(context: context)
+        item.title = "MacBook Pro"
+        item.price = 1800
+        item.details = "I love Mac.. Its so fast and accurate and the trackpad is awesome compared to Windows.."
+        
+        let item1 = Item(context: context)
+        item1.title = "Lenevo"
+        item1.price = 700
+        item1.details = "I love Mac.. Its so fast and accurate and the trackpad is awesome compared to Windows.."
+
+        
+        let item2 = Item(context: context)
+        item2.title = "Dell"
+        item2.price = 500
+        item2.details = "I love Mac.. Its so fast and accurate and the trackpad is awesome compared to Windows.."
+
     }
 }
 
