d3e877effbaae6ab525d975e48fc6aec349289b5
Snehal Sutar <svs.sutar.1@gmail.com>
2016-10-22 17:59:47 -0500

    Function to configure data on item cell for table view cell

--- DreamListerCore/DreamListerCore/ItemCellTableViewCell.swift
+++ DreamListerCore/DreamListerCore/ItemCellTableViewCell.swift
@@ -14,4 +14,10 @@ class ItemCellTableViewCell: UITableViewCell {
     @IBOutlet weak var title: UILabel!
     @IBOutlet weak var price: UILabel!
     @IBOutlet weak var details: UILabel!
+    
+    func configureCell(item: Item){
+        title.text = item.title
+        price.text = "$\(item.price)"
+        details.text = item.details
+    }
 }
