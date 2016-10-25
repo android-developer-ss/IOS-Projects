222921dab6e986fb92846df86fd3b8a35bf33136
Snehal Sutar <svs.sutar.1@gmail.com>
2016-10-24 19:13:57 -0500

    Added controller group and itemdetailVC

---
+++ DreamListerCore/DreamListerCore/ItemDetailsVC.swift
@@ -0,0 +1,24 @@
+//
+//  ItemDetailsVC.swift
+//  DreamListerCore
+//
+//  Created by Snehal Sutar on 10/24/16.
+//  Copyright © 2016 Snehal Sutar. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailsVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
