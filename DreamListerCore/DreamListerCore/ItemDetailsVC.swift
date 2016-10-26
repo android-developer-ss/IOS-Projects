14cf5c8d48fd1398f38d022b5216ee7d11b20232
Snehal Sutar <svs.sutar.1@gmail.com>
2016-10-25 20:59:22 -0500

    Connected ItemDetailsVC functions to main storyboard.

--- DreamListerCore/DreamListerCore/ItemDetailsVC.swift
+++ DreamListerCore/DreamListerCore/ItemDetailsVC.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
-class ItemDetailsVC: UIViewController {
+class ItemDetailsVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var storePicker: UIPickerView!
+    @IBOutlet var titleField: UIView!
+    @IBOutlet weak var priceField: UITextField!
+    @IBOutlet weak var detailsView: UITextField!
+    
+    var stores = [Store]()
+    
     override func viewDidLoad() {
         if let topItem = self.navigationController?.navigationBar.topItem{
             topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+            
+            storePicker.dataSource = self
+            storePicker.delegate = self
         }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let store = stores[row]
+        return store.name
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return stores.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        <#code#>
     }
 }
