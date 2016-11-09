264fdd8a1db4f4655e3782aa77e52b9b5b471254
Snehal Sutar <svs.sutar.1@gmail.com>
2016-11-08 19:53:54 -0600

    Renaming and connecting outlets.

--- WeatherApp/WeatherApp/WeatherVC.swift
+++ WeatherApp/WeatherApp/WeatherVC.swift
@@ -8,8 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherVC: UIViewController {
 
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var currentTemperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var currentWeatherImage: UIImageView!
+    @IBOutlet weak var currentWeatherTypeLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
