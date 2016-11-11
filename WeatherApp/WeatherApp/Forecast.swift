a2ee91d920b348ab119542d56ae3005b2ef692eb
Snehal Sutar <svs.sutar.1@gmail.com>
2016-11-10 20:41:04 -0600

    Forecast model class created and initialized.

---
+++ WeatherApp/WeatherApp/Forecast.swift
@@ -0,0 +1,45 @@
+//
+//  Forecast.swift
+//  WeatherApp
+//
+//  Created by Snehal Sutar on 11/10/16.
+//  Copyright © 2016 Snehal Sutar. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class Forecast {
+    var _date: String!
+    var _weatherType: String!
+    var _highTemp: String!
+    var _lowTemp: String!
+    
+    var date: String {
+        if _date == nil {
+            _date = ""
+        }
+        return _date
+    }
+    
+    var weatherType: String {
+        if _weatherType == nil {
+            _weatherType = ""
+        }
+        return _weatherType
+    }
+    
+    var highTemp: String {
+        if _highTemp == nil {
+            _highTemp = ""
+        }
+        return _highTemp
+    }
+    
+    var lowTemp: String {
+        if _lowTemp == nil {
+            _lowTemp = ""
+        }
+        return _lowTemp
+    }
+}
