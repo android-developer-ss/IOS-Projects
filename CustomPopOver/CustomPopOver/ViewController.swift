d24c5b4d49ab840d314117a9d16c2ecdb196ab3c
Snehal Sutar <svs.sutar.1@gmail.com>
2016-11-14 21:18:34 -0600

    Call to custom popover.

--- CustomPopOver/CustomPopOver/ViewController.swift
+++ CustomPopOver/CustomPopOver/ViewController.swift
@@ -8,18 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "popoverSegue" {
+             let popoverViewController = segue.destination as! UIViewController
+                popoverViewController.modalPresentationStyle = UIModalPresentationStyle.popover
+                popoverViewController.popoverPresentationController!.delegate = self
+            
+        }
+    }
+    
+    func adaptivePresentationStyle(for controller: UIPresentationController) -> UIModalPresentationStyle {
+        return UIModalPresentationStyle.none
+    }
 }
 
